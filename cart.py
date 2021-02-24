76243e2d2edf29ef9e9f00f596529b196b3d7a2f
abhirupdutta25 <62965677+abhirupdutta25@users.noreply.github.com>
2021-02-23 23:07:55 -0800

    Added the constructor

--- cart.py
+++ cart.py
@@ -4,12 +4,24 @@ from flask import Flask, request, flash, url_for, redirect, render_template, jso
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = '_5#y2L"F4Q8z\n\xec]/'
+db = SQLAlchemy(app)
+
 
 # an item
 class Product(db.Model):
     name = db.Column(db.String(100), unique = True)
     img = db.Column(db.String(70), unique = True)
     price = db.Column(db.Float)
+
+def __init__(self, name, img, price):
+   self.name = name
+   self.img = img
+   self.price = price
 
 # show what's in the cart
 @app.route('/cart')
